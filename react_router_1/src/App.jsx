6097e53abdf16b9570545dbba1dc1c593845af9d
Tani-2007 <saxenatanishq4567@gmail.com>
2025-01-06 00:30:38 +0530

    first

--- react_router_1/src/App.jsx
+++ react_router_1/src/App.jsx
@@ -3,24 +3,31 @@ import { About } from "./pages/About";
 import { Movie } from "./pages/Movie";
 import { Contact } from "./pages/Contact";
 import { Home } from "./pages/Home";
+import { AppLayout } from "./components/layouts/AppLayouts";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/about",
-      element: <About />,
-    },
-    {
-      path: "/Movie",
-      element: <Movie />,
-    },
-    {
-      path: "/contact",
-      element: <Contact />,
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/Movie",
+          element: <Movie />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+      ],
     },
   ]);
   return <RouterProvider router={router} />;
